0835018c960bfbe2c29740ab737ed5b3bc7e8a36
Aayush Mankar <mankaraayush18@gmail.com>
2024-02-17 19:07:19 +0530

    commited env file

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT= 5555;
 
-export const mongoDBURL="mongodb://mankaraayush18:Aayush2132@ac-jkcagh9-shard-00-00.mkzqebk.mongodb.net:27017,ac-jkcagh9-shard-00-01.mkzqebk.mongodb.net:27017,ac-jkcagh9-shard-00-02.mkzqebk.mongodb.net:27017/books?ssl=true&replicaSet=atlas-81dvov-shard-0&authSource=admin&retryWrites=true&w=majority"
+export const mongoDBURL=mongoDBURI
